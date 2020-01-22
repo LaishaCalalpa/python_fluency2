c470932139954f25559078551896861877e2de04
EC2 Default User <ec2-user@ip-172-31-24-82.us-east-2.compute.internal>
2020-01-22 15:10:30 +0000

    add

--- python_fluency2.py
+++ python_fluency2.py
@@ -1,23 +1,23 @@
-# new_list = []
-# for i in old_list:
-#     if filter(i):
-#         new_list.append(expressions(i))
-# You can obtain the same thing using list comprehension:
-
-# new_list = [expression(i) for i in old_list if filter(i)]
-
-
-# [ expression for item in list if conditional ]
-# This is equivalent to:
-# for item in list:
-#     if conditional:
-#         expression
+import re
 
 def halved(num_list):
     return [item / 2 for item in num_list]
 print(halved([2, 4, 6, 8]))
 
 def only_positive(num_list):
-    return [item for item in num_list if filter(item > 0, num_list)]
-    
+    return [item for item in num_list if item > 0]
 print(only_positive([-2,-4, 6, 8]))
+
+def total(lst):
+    return sum(lst)
+print(total([2, 4]))
+
+def stripped_strings(lst):
+    return [re.sub("\W", "", string) for string in lst]
+print(stripped_strings(["hello123124.....", "213412isfs"]))
+
+
+
+
+def valid_contacts(lst):
+    return [contact for contact in lst if len(contact.phone_number) == 10]
